0e15533abec2014242cdcf486f83a7c1d52b392a
Ilsa. <ilsasyeda@gmail.com>
2024-03-15 21:55:16 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+let heart = "kind";
+console.log("is heart == kind?\ni think its True");
+console.log(heart == "kind");
+
+let sun = "day";
+console.log(sun == "day"); // true
+
+let dark = "night";
+console.log(dark == "night"); //true
+
+let walk = "daily";
+console.log(walk == "once a week"); //false
+
+let deeds = "right";
+console.log(deeds == "wrong"); //false
